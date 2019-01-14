8645f4704216453031554c49ec72c6b16639ba91
ChaoticWeg <shawn@chaoticweg.cc>
2019-01-14 17:12:04 -0600

    add data load/dump functions

---
+++ lib/data/data.py
@@ -0,0 +1,14 @@
+import json
+
+def load(filepath):
+    result = None
+    try:
+        with open(filepath, 'r') as infile:
+            result = json.load(infile)
+    except FileNotFoundError:
+        pass
+    return result
+
+def dump(data, filepath):
+    with open(filepath, 'w') as outfile:
+        json.dump(data, outfile, sort_keys=False, indent=4)
